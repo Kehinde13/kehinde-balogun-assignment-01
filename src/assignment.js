008020a360c079f3dfd49edc8b33af8476d89ea9
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-08-27 11:15:41 +0000

    completed the assignment

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,14 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for (let i = 1; i <= destination; i++){
+        sum += i
+    }
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -68,11 +71,14 @@ function countEvenNumbersWithin(destination) {
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
     let result = [];
+    arrayOfNumbers.forEach((number) => {
+        result.push(Math.trunc((((number * 9) / 5 + 32) * 10) / 10));
+    })
 
     return result;
 }
 
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 // ========================
 // DO NOT EDIT THIS BLOCK
